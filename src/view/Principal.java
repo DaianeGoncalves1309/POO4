4087b9a58b866385120888d81bc07e81a19a3f13
leope <leope@DESKTOP-5CB9BEC>
2023-08-30 16:13:58 -0300

    commit

---
+++ src/view/Principal.java
@@ -0,0 +1,68 @@
+package view;
+
+import java.util.Scanner;
+
+public class Principal {
+
+
+	public static void main(String[] args) {
+
+		String nomeEmpresa = "Americanas"; 
+		long salarioTotal = 1000000000;
+		int qtdDiasFaturamento = 21;
+		double matematicaB1 = 10;
+		double matematicaB2 = 5;
+		double matematicaB3 = 3;
+		double matematicaB4 = 1;
+		String placa = "AAA2020";
+	    String chassi = "555555555555"; 
+	    String modelo = "Gol"; 
+	    int ano = 2010;
+		String cor = "amarelo"; 
+		String nomeDono = "Joao da Silva"; 
+		String nrDeOuroMatemtica = "1.61803";
+		String aluno1 = "Maria";
+		String aluno2 = "Ana";
+		String aluno3 = "Gabi";
+		String aluno4 = "Jaque";
+		String aluno5 = "Dai";
+		String aluno6 = "Gui";
+		String aluno7 = "Davi";
+		String aluno8 = "Pedro";
+		String aluno9 = "Lucas";
+		String aluno10 = "Beto";
+		int qtDeTenis = 2; 
+	
+	
+	System.out.println("Nome empresa" + " " + nomeEmpresa);
+	System.out.println("Total de salários pagos R$" + salarioTotal);
+	System.out.println("Total de dias de faturamento R$" + qtdDiasFaturamento);
+	System.out.println("Nota de Matemática 1º bimestre" + " " + matematicaB1);
+	System.out.println("Nota de Matemática 2º bimestre" + " " + matematicaB2);
+	System.out.println("Nota de Matemática 3º bimestre" + " " + matematicaB3);
+	System.out.println("Nota de Matemática 4º bimestre" + " " + matematicaB4);
+	System.out.println("O número da placa é" + " " + placa);
+	System.out.println("O número do chassi é" + " " + chassi);
+	System.out.println("O modelo do carro é" + " " + modelo);
+	System.out.println("O ano do carro é" + " " + ano);
+	System.out.println("A cor do carro é" + " " + cor);
+	System.out.println("O nome do dono é" + " " + nomeDono);
+	System.out.println("O número de ouro da matemática é" + " " + nrDeOuroMatemtica);
+	System.out.println("Nome do Aluno(a)" + " " + aluno1);
+	System.out.println("Nome do Aluno(a)" + " " + aluno2);
+	System.out.println("Nome do Aluno(a)" + " " + aluno3);
+	System.out.println("Nome do Aluno(a)" + " " + aluno4);
+	System.out.println("Nome do Aluno(a)" + " " + aluno5);
+	System.out.println("Nome do Aluno(a)" + " " + aluno6);
+	System.out.println("Nome do Aluno(a)" + " " + aluno7);
+	System.out.println("Nome do Aluno(a)" + " " + aluno8);
+	System.out.println("Nome do Aluno(a)" + " " + aluno9);
+	System.out.println("Nome do Aluno(a)" + " " + aluno10);
+	System.out.println("Quantidade de tênis" + " " + qtDeTenis );
+	
+	
+	
+	}
+	
+}
+
